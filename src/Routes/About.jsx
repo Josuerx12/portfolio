17a21195947829ee5b9f7efb02285b0aae659845
josuerx12 <josueaze12@gmail.com>
2023-05-17 23:14:16 -0300

    updatas

--- src/Routes/About.jsx
+++ src/Routes/About.jsx
@@ -7,16 +7,34 @@ const About = () => {
       <div className="sobre-section">
         <h3 className="title">Sobre</h3>
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industrys standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
+        Meu nome é Josué Carvalho, sou um desenvolvedor front-end 
+        em transição de carreira, com 21 anos de idade e possuo 
+        habilidades avançadas em inglês. Atualmente, estou focado 
+        em aprimorar minhas habilidades em React JS, uma das 
+        principais bibliotecas JavaScript para o desenvolvimento 
+        de interfaces de usuário.
         </p>
+        <p>
+        Minha formação em fisioterapia traz uma perspectiva 
+        única para o meu trabalho como desenvolvedor front-end, 
+        permitindo que eu crie interfaces intuitivas e acessíveis 
+        para os usuários finais. Estou constantemente em busca de 
+        maneiras de aprimorar minhas habilidades técnicas e estou 
+        animado para encontrar oportunidades desafiadoras, onde eu 
+        possa contribuir para projetos inovadores e colaborar em equipe, 
+        visando alcançar resultados excepcionais.
+        </p>
+      <div className="skills">
+        <h3 className="title">Habilidades</h3>
+        <div className="technologies">
+          <img src="https://raw.githubusercontent.com/Josuerx12/Pagina-Portifolio-Curso-JS-Avancado/master/data/imgs/html.png" alt="html skill" />
+          <img src="https://raw.githubusercontent.com/Josuerx12/Pagina-Portifolio-Curso-JS-Avancado/master/data/imgs/css.png" alt="css skill" />
+          <img src="https://raw.githubusercontent.com/Josuerx12/Pagina-Portifolio-Curso-JS-Avancado/master/data/imgs/sass.png" alt="sass skill" />
+          <img src="https://raw.githubusercontent.com/Josuerx12/Pagina-Portifolio-Curso-JS-Avancado/master/data/imgs/js.png" alt="js skill" />
+          <img src="https://raw.githubusercontent.com/Josuerx12/Pagina-Portifolio-Curso-JS-Avancado/master/data/imgs/react.png" alt="react skill" />
+          <img src="https://raw.githubusercontent.com/Josuerx12/Pagina-Portifolio-Curso-JS-Avancado/master/data/imgs/git.png" alt="git skill" />
+        </div>
+      </div>
       </div>
     </main>
   );
--- src/Routes/Projects.jsx
+++ src/Routes/Projects.jsx
@@ -1,129 +1,51 @@
 import { useState, useEffect } from "react";
 import { FaGithub } from "react-icons/fa";
-import weatherAppPic from "../assets/imgs/weatherApp.png";
 import "./Projects.sass";
 import axios from "axios";
 
 const Projects = () => {
-  //   const [dados, setDados] = useState([]);
+  
+  const [data , setData] = useState([])
 
-  //   const getData = async () => {
-  //     try {
-  //       const res = await axios.get(
-  //         "https://raw.githubusercontent.com/Josuerx12/josuerx12/main/dados.json"
-  //       );
-  //       const data = res.data;
-  //       setDados(data);
-  //     } catch {
-  //       console.log("Erro na coleta dos dados!!!");
-  //     }
-  //   };
+  const getData = async () => {
+    axios.get('https://raw.githubusercontent.com/Josuerx12/josuerx12/main/dados.json')
+    .then( res => setData(res.data))
+    .catch(err => console.log(err))
+  }
 
-  //   useEffect(() => {
-  //     getData();
-  //   }, []);
+  useEffect(()=> {
+    getData()
+  },[])
 
+  const portfolio = data.portfolio.map((p, i)=> (
+    <div key={i} className="project">
+      <img className="project-img" src={p.img} alt={p.name} />
+      <div className="project-desc">
+        <h3>{p.name}</h3>
+        <p>{p.desc}</p>
+        <a
+          className="vizualizar"
+          href={p.pageUrl}
+          target="_blank"
+          rel="noreferrer"
+        >
+              Visitar Aplicação
+        </a>
+        <a
+          href={p.url}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Código <FaGithub />
+        </a>
+      </div>
+    </div>
+  ))
+  
   return (
     <main className="projects">
       <h6 className="title">Projetos</h6>
-      <div className="project">
-        <img className="project-img" src={weatherAppPic} alt="Project image" />
-        <div className="project-desc">
-          <h3>Weather APP</h3>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industrys standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-          </p>
-          <a
-            className="vizualizar"
-            href="https://josuerx12.github.io/WheaterAPP/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Visitar Aplicação
-          </a>
-          <a
-            href="https://github.com/Josuerx12/WheaterAPP"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Código <FaGithub />
-          </a>
-        </div>
-      </div>
-
-      <div className="project">
-        <img className="project-img" src={weatherAppPic} alt="Project image" />
-        <div className="project-desc">
-          <h3>Weather APP</h3>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industrys standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-          </p>
-          <a
-            className="vizualizar"
-            href="https://josuerx12.github.io/WheaterAPP/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Visitar Aplicação
-          </a>
-          <a
-            href="https://github.com/Josuerx12/WheaterAPP"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Código <FaGithub />
-          </a>
-        </div>
-      </div>
-
-      <div className="project">
-        <img className="project-img" src={weatherAppPic} alt="Project image" />
-        <div className="project-desc">
-          <h3>Weather APP</h3>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industrys standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-          </p>
-          <a
-            className="vizualizar"
-            href="https://josuerx12.github.io/WheaterAPP/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Visitar Aplicação
-          </a>
-          <a
-            href="https://github.com/Josuerx12/WheaterAPP"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Código <FaGithub />
-          </a>
-        </div>
-      </div>
+      {portfolio}
     </main>
   );
 };
